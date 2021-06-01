63411530dee414fec1db470fb069fb14c1995aa1
tos97 <47986842+tos97@users.noreply.github.com>
2021-06-01 13:07:07 +0200

    modifica di una cosa inutile

--- src/main/java/com/tosiani/Steps.java
+++ src/main/java/com/tosiani/Steps.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-
-
 import static com.tosiani.Utility.Utils.valoreProp;
 
 public class Steps {
